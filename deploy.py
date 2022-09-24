5159c3796467ed74ef1c14c17fc5a857a08c3b44
Drelf2018 <drelf2018@outlook.com>
2022-09-24 23:11:32 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,16 +6,4 @@ with open('.\\dist\\index.html', 'r', encoding='utf-8') as fp:
     with open('.\\dist\\index.html', 'w', encoding='utf-8') as f:
         f.write(content)
 
-cmd = '''cd dist
-move .\\_assets\\* .
-rmdir /Q _assets
-
-git init
-git add -A
-git commit -m deploy
-git push -f git@github.com:drelf2018/nana7mi.link.git master:gh-pages
-cd ..'''
-
-for c in cmd.split('\n'):
-    os.system(c)
-    input()
+os.system('deploy.bat')
